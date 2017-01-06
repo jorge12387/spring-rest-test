f9b8beb18adfad8d268d7930822c7b05a2a2472c
jorge12387 <jorge.aniez@gmail.com>
2017-01-06 10:32:37 -0400

    new commit

--- src/main/java/org/spring/test/socket/MySocket.java
+++ src/main/java/org/spring/test/socket/MySocket.java
@@ -1,7 +1,34 @@
 package org.spring.test.socket;
 
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
 /**
  * Created by user on 1/6/2017.
  */
+@ServerEndpoint(value = "/game")
 public class MySocket {
+
+    @OnOpen
+    public void onOpen(Session session) {
+
+    }
+
+    @OnMessage
+    public String onMessage(String message, Session session) {
+        if (message.equals("quit")) {
+                try {
+                    session.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Game ended"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+        }
+        return message;
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+
+    }
 }
